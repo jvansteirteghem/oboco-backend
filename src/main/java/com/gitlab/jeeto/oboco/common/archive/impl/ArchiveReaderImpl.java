ae20e284dd3df771eb3f303ee3896aeb9e909e7e
jeeto jeeto <jeeto.jeeto@gmail.com>
2021-01-11 16:21:29 +0100

    fix order

--- src/main/java/com/gitlab/jeeto/oboco/common/archive/impl/ArchiveReaderImpl.java
+++ src/main/java/com/gitlab/jeeto/oboco/common/archive/impl/ArchiveReaderImpl.java
@@ -10,9 +10,9 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.github.paour.natorder.NaturalOrderComparator;
 import com.gitlab.jeeto.oboco.common.FileType;
 import com.gitlab.jeeto.oboco.common.FileWrapper;
+import com.gitlab.jeeto.oboco.common.NaturalOrderComparator;
 import com.gitlab.jeeto.oboco.common.archive.ArchiveReader;
 
 import net.sf.sevenzipjbinding.ExtractOperationResult;
