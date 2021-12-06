b1b7855cf5f85ddb1e507e4c9622261227471f34
jeeto jeeto <jeeto.jeeto@gmail.com>
2021-12-06 10:41:16 +0100

    fix log

--- src/main/java/com/gitlab/jeeto/oboco/common/exception/ProblemDtoExceptionMapper.java
+++ src/main/java/com/gitlab/jeeto/oboco/common/exception/ProblemDtoExceptionMapper.java
@@ -7,33 +7,32 @@ import javax.ws.rs.ext.ExceptionMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public abstract class ProblemDtoExceptionMapper implements ExceptionMapper<Exception> {
+public abstract class ProblemDtoExceptionMapper implements ExceptionMapper<Throwable> {
 	private static Logger logger = LoggerFactory.getLogger(ProblemDtoExceptionMapper.class.getName());
 	
 	public abstract Response getResponse(ProblemDto problemDto);
 	
 	@Override
-	public Response toResponse(Exception e) {
+	public Response toResponse(Throwable t) {
 		ProblemDto problemDto = null;
 		
-		if(e instanceof ProblemException) {
-			ProblemException ee = (ProblemException) e;
-			Problem problem = ee.getProblem();
+		if(t instanceof ProblemException) {
+			ProblemException e = (ProblemException) t;
+			Problem problem = e.getProblem();
 			
 			problemDto = new ProblemDto();
 			problemDto.setStatusCode(problem.getStatusCode());
 			problemDto.setCode(problem.getCode());
 			problemDto.setDescription(problem.getDescription());
-		} else if(e instanceof WebApplicationException) {
-			logger.error("Error.", e);
+		} else if(t instanceof WebApplicationException) {
+			WebApplicationException e = (WebApplicationException) t;
 			
-			WebApplicationException wae = (WebApplicationException) e;
 			problemDto = new ProblemDto();
-			problemDto.setStatusCode(wae.getResponse().getStatus());
+			problemDto.setStatusCode(e.getResponse().getStatus());
 			problemDto.setCode("PROBLEM");
 			problemDto.setDescription("Problem.");
 		} else {
-			logger.error("Error.", e);
+			logger.error("Error.", t);
 			
 			problemDto = new ProblemDto();
 			problemDto.setStatusCode(500);
